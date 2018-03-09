71d161ebf1ac902992c88da2db2ba881354eae08
ropaolinelli <ro.paolinelli@hotmail.com>
2018-03-09 12:08:34 -0300

    recuperatorio practica

--- 9-Parciales/recuperatorio/seis.js
+++ 9-Parciales/recuperatorio/seis.js
@@ -1,4 +1,34 @@
 function Mostrar()
 {
+    var peso;
+    var max;
+    var min;
+    var primera=true;
 
+    for(i=0 ; i<3 ; i++)
+    {  peso= prompt("Ingrese el peso")
+        peso= parseInt(peso) 
+    
+        while(peso<0)
+        {peso=prompt("Ingrese el peso correctamente")
+        peso=parseInt(peso)}
+
+        if(primera)
+        { 
+            primera=false
+            max=peso
+            min=peso
+        }
+
+        else
+        {
+            if (peso > max)
+            {max=peso}
+            else (peso < min)
+            { min = peso}
+        }
+    
+    
+    }
+alert("El peso maximo fue " + max + " y  el peso minimo fue " + min)
 }
