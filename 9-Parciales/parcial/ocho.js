e33cd3d97baa7fbc3c233df0592a6e825e725d5d
Rocio Paolinelli <ro.paolinelli@hotmail.com>
2018-03-11 18:34:26 -0300

    8
    8 listo

--- 9-Parciales/parcial/ocho.js
+++ 9-Parciales/parcial/ocho.js
@@ -2,26 +2,73 @@ function Mostrar()
 {
     var contador=0;
     var respuesta="continuar"
-    var temperatura
+    var temperatura 
     var nombre
+    var tempPAR=0
+    var tempIMPA=0
+    var tempMinima
+    var tempMaxima
+    var tempMaxNombre
+    var tempMinNombre
 
-    while (respuesta != "no")//Esta es la llave de while
-    {
+
+    while (respuesta != "no")
+    //Esta es la llave de while
+    {   
         temperatura=prompt("Ingrese temperatura")
         temperatura=parseInt(temperatura)
+        contador++
+
         while(temperatura<-100 || temperatura >100 || isNaN(temperatura))
         {
         temperatura=prompt("Ingrese temperatura")
         temperatura=parseInt(temperatura)
         }
+
         nombre=prompt("Ingrese su nombre")
         while (! isNaN(nombre))
         {
             nombre=prompt("Ingrese su nombre")
         }
 
+        if (contador== 1 )
+        {
+            tempMinima= temperatura
+            tempMinNombre= nombre
+            tempMaxima= temperatura
+            tempMaxNombre = nombre
+        }
 
+        else 
+        {
+            if ( temperatura < tempMinima )
+            {
+                tempMinima = temperatura
+                tempMinNombre = nombre
+            }
 
+            if (temperatura > tempMaxima)
+            {
+                tempMaxima = temperatura
+                tempMaxNombre = nombre
+            }
+        }
+
+        if (temperatura % 2 == 0 && temperatura != 0)
+        {
+            tempPAR ++ 
+
+        }
+        else 
+        {
+            if (temperatura!= 0 )
+            {
+                tempIMPA++
+            }
+        }
+
+         respuesta=prompt("Ingrese no para salir")
     }   
-    respuesta=prompt("Ingrese no para salir")
+
+   alert("La temperatura maxima y el nombre del lugar es: " + tempMaxima + tempMaxNombre + " / La temperatura minima y el nombre del lugar es " + tempMinima + tempMinNombre + " / Hubo " + tempPAR + " pares y " + tempIMPA + " de imares" )
 }
